c9c745a5dbcb5df10c41ffa84bc984dcb7ea5c08
Deva <dvrajan12@gmail.com>
2015-01-03 23:57:12 +0530

    moving config to yml

--- config.rb
+++ config.rb
@@ -1,1 +1,19 @@
-AWS.config(access_key_id: ENV['AWS_ACCESS_KEY'], secret_access_key: ENV['AWS_SECRET_KEY'], region: 'us-west-2')
+require 'yaml'
+
+class Conf
+	@@config_file	
+	def self.load_file
+		@@config_file = YAML.load_file('config.yml')
+	end
+
+	def self.get(key)
+		return @@config_file.fetch(key)
+	end
+end
+
+Conf.load_file
+AWS.config(access_key_id: ENV['AWS_ACCESS_KEY'], secret_access_key: ENV['AWS_SECRET_KEY'], region: Conf.get('aws_region'))
+
+QUEUE_NAME = Conf.get('queue_name')
+
+MESSAGE = Conf.get('message_to_send')
--- producer.rb
+++ producer.rb
@@ -6,12 +6,12 @@ require_relative 'config'
 sqs = AWS::SQS.new
 
 puts "Acquiring queue..."
-q = sqs.queues.create 'sample'
+q = sqs.queues.create QUEUE_NAME
 puts "Queue acquired!"
 
 number_of_messages.to_i.times do |i|
 	puts "Sending message #{i+1}"
-	q.send_message 'HELO'
+	q.send_message MESSAGE
 	if i+1 != number_of_messages.to_i
 		puts "Waiting for #{interval} seconds..."
         	sleep(interval.to_i)
