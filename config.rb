ec6dd29bedaa53bccf64464b9a9be8a8fc406e1d
Deva <dvrajan12@gmail.com>
2015-01-04 12:55:52 +0530

    Using default aws credentails provider

--- config.rb
+++ config.rb
@@ -12,8 +12,6 @@ class Conf
 end
 
 Conf.load_file
-AWS.config(access_key_id: ENV['AWS_ACCESS_KEY'], secret_access_key: ENV['AWS_SECRET_KEY'], region: Conf.get('aws_region'))
-
+AWS.config(region: Conf.get('aws_region'))
 QUEUE_NAME = Conf.get('queue_name')
-
 MESSAGE = Conf.get('message_to_send')
